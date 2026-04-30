470b1dd25f8d9794b44087884512361165013fe6
dida0982 <dida0982@gmail.com>
2026-04-30 05:36:58 -0300

    feat: insert a paragraph to the  HTML
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 let title = document.querySelector( 'h1' );
 title.innerHTML = 'Guessing Game';
+
+let paragraph = document.querySelector( 'p' );
+paragraph.innerHTML = 'Choose a number between 1 and 10.';
