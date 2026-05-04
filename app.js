a13d1a3087fbdd31ad9c1c4895cc2ec413e31039
dida0982 <dida0982@gmail.com>
2026-05-04 12:12:05 -0300

    feat: add return the let secretNumber
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-function displayTextOnScreen(tag, text) {
+let secretNumber = randomNumber()
+
+function displayTextOnScreen ( tag, text )
+{
     let field = document.querySelector( tag );
     field.innerHTML = text;
 }
 
 displayTextOnScreen( 'h1', 'Game of secret number.' );
 displayTextOnScreen( 'p', 'Choose a number the 1 and 10.' );
-function checkKick(params) {
+function checkKick() {
     console.log('The button was clicked ');
 
 }
 
-function randomNumber(params) {
-    parseInt( Math.random() * 10 + 1 );
+function randomNumber() {
+    return parseInt( Math.random() * 10 + 1 );
 }
