ab1abe6525070eaff29bf4d5724a2f004473192f
dida0982 <dida0982@gmail.com>
2026-05-04 12:05:35 -0300

    refactore: excludes commented code
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// let title = document.querySelector( 'h1' );
-// title.innerHTML = 'Guessing Game';
-
-// let paragraph = document.querySelector( 'p' );
-// paragraph.innerHTML = 'Choose a number between 1 and 10.';
-
-
 function displayTextOnScreen(tag, text) {
     let field = document.querySelector( tag );
     field.innerHTML = text;
