6db03ac7394baf68b42fcd1c68897ee4f6f05f98
dida0982 <dida0982@gmail.com>
2026-05-04 12:01:19 -0300

    feat: add function of random number
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ function displayTextOnScreen(tag, text) {
     field.innerHTML = text;
 }
 
-displayTextOnScreen( 'h1', 'Game of secret number .' );
+displayTextOnScreen( 'h1', 'Game of secret number.' );
 displayTextOnScreen( 'p', 'Choose a number the 1 and 10.' );
 function checkKick(params) {
     console.log('The button was clicked ');
 
+}
+
+function randomNumber(params) {
+    parseInt( Math.random() * 10 + 1 );
 }
